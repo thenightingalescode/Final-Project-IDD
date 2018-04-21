3eef506a6c2fc1f59b833f08447b98e5a3319462
Remi <Remi.gurak@gmail.com>
2018-04-21 13:14:56 -0400

    Initial commit

---
+++ web-starter/js/bucket.js
@@ -0,0 +1,20 @@
+$( document ).ready(function() {
+  $( "#tabs" ).tabs();
+
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:5
+        },
+        1000:{
+            items:7
+        }
+    }
+})
+});
