09e51295cc4a74f7baa53b5448d9af2f1d7e9ab3
Remi <Remi.gurak@gmail.com>
2018-04-23 22:06:58 -0400

    Update of IDD Final

--- web-starter/js/bucket.js
+++ web-starter/js/bucket.js
@@ -1,10 +1,22 @@
 $( document ).ready(function() {
+
+  $("#profile img").css({
+      "opacity":".5"
+  });
+
+  $("#profile img").on("click", function() {
+    $(this).css({
+        "opacity": "1"
+    });
+  });
+
   $( "#tabs" ).tabs();
 
   $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
-    nav:true,
+    nav:false,
+    dots:true,
     responsive:{
         0:{
             items:3
